3a38fa4b6c5c769c91ccd1eb9d8e11d240f833dd
Milos Popovic <milos_popovic@outlook.com>
2020-05-16 05:45:47 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 # code your #position_taken? method here!
 def position_taken?(array, index)
-  if array[index] == " "
+  if array[index] == " " || array[index] == ""
     return false
   else
     return true
